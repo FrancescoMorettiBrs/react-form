4f8c68556d72ed8a767d19077bb7e0ddaff4e62a
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-06-06 16:08:49 +0200

    feat: task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import data from '../data.js'
+import { use, useState } from "react";
+import data from "../data.js";
 
- const App = () => {
+const App = () => {
+  const [list, setList] = useState(data);
+  const [newTitle, setNewTitle] = useState("");
+
+  const gestisciSubmit = (event) => {
+    event.preventDefault();
+    if (newTitle.trim() === "") return;
+
+    const newItem = {
+      id: list.length + 1,
+      title: newTitle,
+    };
+
+    setList([...list, newItem]);
+    setNewTitle("");
+  };
+
   return (
-    <>
-      {data.map((curData) => {
-        return (<h2 key={curData.id}>{curData.titolo}</h2>)
-      })}
-    </>
-  )
-}
+    <div>
+      <form onSubmit={gestisciSubmit}>
+        <input type="text" value={newTitle} onChange={(event) => setNewTitle(event.target.value)} placeholder="Inserisci il titolo" />
+        <button type="submit">Aggiungi titolo</button>
+      </form>
+      <div>
+        {list.map((curList) => {
+          return <h2 key={curList.id}>{curList.title}</h2>;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default App;
